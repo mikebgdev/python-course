06ede55d584dd2f5a420f6b991df16a1e361072c
Michael Ballester <mike@mikebgdev.com>
2023-09-14 22:37:06 +0200

    lessons 1 and 2

---
+++ beginner/1-helloWorld.py
@@ -0,0 +1,5 @@
+print("Hola Python")
+print('Hola Python 2')
+
+
+print(type("Hola Python type"))
---
+++ beginner/2-vars.py
@@ -0,0 +1,23 @@
+my_string_var = "My Var String"
+print(my_string_var)
+
+my_int_var = 5
+print(my_int_var)
+
+my_int_to_str_var = str(my_int_var)
+print(my_int_to_str_var)
+print(type(my_int_to_str_var))
+
+my_bool_var = False
+print(my_bool_var)
+
+print(my_string_var, my_int_to_str_var, my_bool_var)
+
+print(len(my_string_var))
+print(len(my_int_to_str_var))
+
+test: str = "test"
+test = 45
+
+print(test)
+print(type(test))
